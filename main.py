4c10c14f5ac4f9bc9eea01ea57a0a6a03e2f1321
dimon-ton <saichon2namoom@gmail.com>
2023-09-27 17:47:48 +0700

    edit for english pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from gtts import gTTS
 from googletrans import Translator
+from pydub import AudioSegment
+from tqdm import tqdm
 
 
 def translate(word):
@@ -8,24 +10,42 @@ def translate(word):
 
 	return meaning
 
-txtWord = 'property'
-translateWord = translate(txtWord)
+
+def pronunEng(word):
+	tss = gTTS(text=word, lang='en')
+	
+	tss.save('engDemo.mp3')
 
 
-txt = 'สะกดคำว่า {} ... ... คือ'.format(txtWord)
+def pronunThai(word):
+	tss = gTTS(text=word, lang='th')
+	
+	tss.save('thaiDemo.mp3')
 
-for t in txtWord:
-	txt += " "
-	txt += " ...  ...  "
-	txt += t
 
-txt += " .... ... "
 
-txt += "อ่านว่า ... ... " + txtWord
-txt += "... ...  มีความหมายว่า ... ... {}".format(translateWord)
 
-print(txt)
 
-tts = gTTS(text=txt, lang='th')
+# translate the meaning of the word
+txtWord = 'door'
+translatedWord = translate(txtWord)
 
-tts.save('demo.mp3')
+
+
+sentences = ["ภาษาอังกฤษวันละคำ วันนี้ขอเสนอคำว่า ", txtWord]
+
+
+pronunThai(sentences[0])
+pronunEng(sentences[1])
+
+
+
+
+sound1 = AudioSegment.from_file(r'C:\Users\saich\Documents\EngPrac\engDemo.mp3', format='mp3')
+sound2 = AudioSegment.from_file(r'C:\Users\saich\Documents\EngPrac\thaiDemo.mp3', format='mp3')
+
+
+combined = sound1 + sound2
+
+combined.export(r'C:\Users\saich\Documents\EngPrac\exported.mp3', format='mp3')
+
